adf4e0beeee3f977c10d9ecd597c055f055f5382
Joao Gilberto Magalhaes <joao@byjg.com.br>
2024-05-18 18:02:58 -0500

    Add EasyHAProxy

---
+++ stacks/easyhaproxy/upgrade.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add byjg https://opensource.byjg.com/helm > /dev/null 2>&1
+helm repo update > /dev/null 2>&1
+
+################################################################################
+# Pre-requisites
+################################################################################
+
+# Check if there is any node with the label "easyhaproxy/node=master"
+master_node_exists=$(kubectl get nodes --selector=easyhaproxy/node=master -o jsonpath='{.items[*].metadata.name}')
+
+# If there is no node with the label, then label the first node
+if [ -z "$master_node_exists" ]; then
+    masternode=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+    kubectl label nodes $(masternode) "easyhaproxy/node=master" --overwrite
+fi
+
+################################################################################
+# chart
+################################################################################
+STACK="ingress"
+CHART="byjg/easyhaproxy"
+NAMESPACE="easyhaproxy"
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values"
